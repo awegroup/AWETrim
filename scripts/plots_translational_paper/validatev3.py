0a473f290e7c49f1d7d15886eaa0d86fc6a65ddc
ocayon <94445351+ocayon@users.noreply.github.com>
2025-02-13 17:36:05 +0100

    add validation v3 to translational paper

---
+++ scripts/plots_translational_paper/validatev3.py
@@ -0,0 +1,283 @@
+import h5py
+import pandas as pd
+import numpy as np
+from picawe import SystemModel
+import casadi as ca
+import time
+
+
+def read_results(year, month, day, kite_model, addition="", path_to_main=""):
+    path = "data/flight_logs/"
+    date = str(year) + "-" + str(month) + "-" + str(day)
+    file_name = str(kite_model) + "_" + date
+    hdf5_path = path_to_main + path + file_name + addition + ".h5"
+    ekf_output_df, flight_data_df, config_data = read_results_from_hdf5(hdf5_path)
+    return ekf_output_df, flight_data_df, config_data
+
+
+def read_results_from_hdf5(hdf5_path):
+    with h5py.File(hdf5_path, "r") as hf:
+        # Read the ekf_output_df DataFrame
+        ekf_group = hf["ekf_output"]
+        ekf_output_df = pd.DataFrame(
+            {
+                col: (
+                    ekf_group[col][:].astype(str)
+                    if ekf_group[col].dtype.kind == "S"
+                    else ekf_group[col][:]
+                )
+                for col in ekf_group.keys()
+            }
+        )
+
+        # Read the flight_data DataFrame
+        flight_group = hf["flight_data"]
+        flight_data_df = pd.DataFrame(
+            {
+                col: (
+                    flight_group[col][:].astype(str)
+                    if flight_group[col].dtype.kind == "S"
+                    else flight_group[col][:]
+                )
+                for col in flight_group
+                if isinstance(flight_group[col], h5py.Dataset)
+            }
+        )
+
+        # Read config_data
+        config_group = hf["config_data"]
+        config_data = read_dict_from_group(config_group)
+
+    return ekf_output_df, flight_data_df, config_data
+
+
+def read_dict_from_group(group):
+    config_dict = {}
+    for key, value in group.attrs.items():
+        if isinstance(value, bytes):
+            value = value.decode("utf-8")  # Decode byte strings back to regular strings
+        config_dict[key] = value
+
+    for subgroup_name in group:
+        subgroup = group[subgroup_name]
+        config_dict[subgroup_name] = read_dict_from_group(subgroup)
+
+    return config_dict
+
+
+results, flight_data, config_data = read_results("2019", "10", "08", "v3", addition="")
+mask = flight_data.cycle.isin([65])
+mask = mask & (flight_data.tether_reelout_speed > 0.2)
+flight_data = flight_data[mask]
+results = results[mask]
+results = results.reset_index(drop=True)
+flight_data = flight_data.reset_index(drop=True)
+
+csv_file = "./processed_data/VSM_results_alpha_sweep.csv"
+v3_polar_data = pd.read_csv(csv_file)
+
+# -----------------------------------------
+# Define the system
+# -----------------------------------------
+
+import json
+
+# -----------------------------------------------
+# Load data and define aerodynamic model
+# -----------------------------------------------
+
+# Define aerodynamic input
+file_path = "./data/v3_aero_input.json"
+with open(file_path, "r") as file:
+    aero_input = json.load(file)
+
+# Example Usage
+kite_model = SystemModel(mass_wing=15, area_wing=20, mass_kcu=25, aero_input=aero_input, dof=3, quasi_steady=True)
+
+
+
+
+solutions = []
+
+start = time.time()
+vw_window = []
+vw_averaged = []
+wdir_window = []
+
+unknown_vars = [
+    "tension_tether_ground",
+    "input_steering",
+    "speed_tangential",
+]
+
+sideslip_func = kite_model.extract_function("angle_sideslip")
+# T_func = kite_model.extract_function("tension_tether")
+solver_options = {
+    "ipopt": {
+        "print_level": 0,  # Suppresses IPOPT output
+        # 'max_iter': 200,  # Maximum number of iterations
+        "sb": "yes",  # Suppresses more detailed solver information
+    },
+    "print_time": False,  # Disables CasADi's internal timing output
+}
+# Sliding window size
+window_size = 5
+position = np.array(
+    [results.kite_position_x, results.kite_position_y, results.kite_position_z]
+).T
+velocity = np.array(
+    [results.kite_velocity_x, results.kite_velocity_y, results.kite_velocity_z]
+).T
+distance_radial = np.linalg.norm(position, axis=1)
+speed_tangential = np.linalg.norm(velocity, axis=1)
+qs_guess = [1e2, 0, 40]
+kite_model.establish_residual()
+solve_func, inputs_name = kite_model.solve_quasi_steady_state(
+        unknown_vars, solver_options=solver_options
+    )
+print(solve_func)
+for i, row in flight_data.iterrows():
+
+    # Wind speed (vw) sliding window average
+    vw_window.append(results.wind_speed_horizontal[i])
+    wdir_window.append(results.wind_direction[i])
+    if len(vw_window) > window_size:
+        vw_window.pop(0)  # Keep the window size constant
+        wdir_window.pop(0)
+    print(i)
+    vw = np.mean(vw_window)  # Compute the average of the current window
+
+    current_state = {
+        "distance_radial": distance_radial[i],
+        "angle_course": row.kite_course,
+        "speed_radial": row.tether_reelout_speed,
+        "angle_azimuth": row.kite_azimuth,
+        "angle_elevation": row.kite_elevation,
+        "speed_wind": vw,
+        "timeder_angle_course": row.kite_yaw_rate_1,
+        "input_depower": row.up,
+    }
+
+    p = [current_state[name] for name in inputs_name]
+    # print(p)
+    lbx,ubx,lbg,ubg = kite_model.get_boundaries(unknown_vars)
+    # print(lbx,ubx,lbg,ubg)
+    sol = solve_func(x0=qs_guess, p=p, lbx=lbx, ubx=ubx, lbg=lbg, ubg=ubg)
+
+    qs_guess = sol["x"]
+    qs_state = {name: float(qs_guess[i]) for i, name in enumerate(unknown_vars)}
+
+    state_combined = {**qs_state, **current_state}
+    sideslip = sideslip_func(
+        *[state_combined[name] for name in sideslip_func.name_in()]
+    )
+    state_combined["sideslip"] = float(sideslip)
+    # state_combined["tension_tether"] = float(T)
+    solutions.append(state_combined)
+    # print(f"Solution: {solution}")
+
+
+
+end = time.time()
+print(f"Time taken: {end - start} seconds for {len(flight_data)} iterations")
+
+
+
+# At dt of 0.1, the time taken is:
+time_per_iteration = (end - start) / (len(flight_data))
+print(f"Time per iteration: {time_per_iteration} seconds")
+
+# Display the solutions
+
+solutions_df = pd.DataFrame(solutions)
+# Filter out rows where 'T' is None
+solutions_df = solutions_df[solutions_df["tension_tether_ground"].notna()]
+
+dt = 0.1
+total_time = len(flight_data) * dt
+# print('Estimated power: ', sum(solutions_df['T']*solutions_df['v_r']*dt)/total_time, 'W')
+# print('Measured power: ', sum(flight_data['ground_tether_force']*flight_data['tether_reelout_speed']*dt)/total_time, 'W')
+
+
+import matplotlib.pyplot as plt
+
+plt.figure()
+plt.plot(speed_tangential, label="tangential speed")
+plt.plot(solutions_df["speed_tangential"], label="speed_tangential")
+plt.legend()
+
+plt.figure()
+plt.plot(flight_data["ground_tether_force"], label="ground_tether_force")
+plt.plot(solutions_df["tension_tether_ground"], label="estimated tension_tether")
+plt.legend()
+
+# plt.figure()
+# plt.plot(np.degrees(solutions_df["angle_roll"]), label="roll tether-kite")
+# plt.plot(np.degrees(solutions_df["angle_pitch"]), label="pitch tether-kite")
+# plt.plot(np.degrees(solutions_df["angle_yaw"]), label="yaw tether-kite")
+# plt.legend()
+# plt.show()
+
+# Plot sideslip
+plt.figure()
+plt.plot(np.degrees(solutions_df["sideslip"]), label="sideslip")
+plt.legend()
+
+
+# PLot steering input
+plt.figure()
+plt.plot(flight_data["us"], label="us measured")
+plt.plot(solutions_df["input_steering"], label="u_s")
+plt.legend()
+# plt.show()
+
+
+# plt.figure()
+# plt.plot(results["wing_lift_coefficient"], label='wing_lift_coefficient')
+# plt.plot(solutions_df['CL'], label='CL')
+# plt.legend()
+# # plt.show()
+
+# plt.figure()
+# plt.plot(results["wing_drag_coefficient"], label='wing_drag_coefficient')
+# plt.plot(solutions_df['CD'], label='CD')
+# plt.legend()
+# plt.show()
+
+total_power = (
+    sum(solutions_df["tension_tether_ground"] * solutions_df["speed_radial"] * dt) / total_time
+)
+measured_power = (
+    sum(flight_data["ground_tether_force"] * flight_data["tether_reelout_speed"] * dt)
+    / total_time
+)
+print("Estimated power: ", total_power, "W")
+print("Measured power: ", measured_power, "W")
+
+
+fig, axs = plt.subplots(1, 2, figsize=(10, 4), sharey=True)
+scatter = axs[0].scatter(
+    solutions_df["angle_azimuth"],
+    solutions_df["angle_elevation"],
+    c=solutions_df["speed_tangential"],
+    cmap="viridis",
+    s=10,
+    vmin = 10,
+    vmax = 30
+)  # `s` adjusts marker size
+
+# Correct way to add a colorbar
+cbar = fig.colorbar(scatter, ax=axs[0])
+cbar.set_label("Tangential Speed [m/s]", fontsize=12)
+
+
+scatter = axs[1].scatter(
+    flight_data["kite_azimuth"],
+    flight_data["kite_elevation"],
+    c=speed_tangential,
+    cmap="viridis",
+    s=10,
+    vmin = 10,
+    vmax = 30
+)  # `s` adjusts marker size
+plt.show()
---
+++ scripts/plots_translational_paper/validatev3_pattern.py
@@ -0,0 +1,204 @@
+import h5py
+import pandas as pd
+import numpy as np
+from picawe import SystemModel
+import casadi as ca
+import time
+from picawe.timeseries.phase import PhaseParameterized
+from picawe.kinematics.parametrized_patterns import FigureEight
+import matplotlib.pyplot as plt
+
+
+def read_results(year, month, day, kite_model, addition="", path_to_main=""):
+    path = "data/flight_logs/"
+    date = str(year) + "-" + str(month) + "-" + str(day)
+    file_name = str(kite_model) + "_" + date
+    hdf5_path = path_to_main + path + file_name + addition + ".h5"
+    ekf_output_df, flight_data_df, config_data = read_results_from_hdf5(hdf5_path)
+    return ekf_output_df, flight_data_df, config_data
+
+
+def read_results_from_hdf5(hdf5_path):
+    with h5py.File(hdf5_path, "r") as hf:
+        # Read the ekf_output_df DataFrame
+        ekf_group = hf["ekf_output"]
+        ekf_output_df = pd.DataFrame(
+            {
+                col: (
+                    ekf_group[col][:].astype(str)
+                    if ekf_group[col].dtype.kind == "S"
+                    else ekf_group[col][:]
+                )
+                for col in ekf_group.keys()
+            }
+        )
+
+        # Read the flight_data DataFrame
+        flight_group = hf["flight_data"]
+        flight_data_df = pd.DataFrame(
+            {
+                col: (
+                    flight_group[col][:].astype(str)
+                    if flight_group[col].dtype.kind == "S"
+                    else flight_group[col][:]
+                )
+                for col in flight_group
+                if isinstance(flight_group[col], h5py.Dataset)
+            }
+        )
+
+        # Read config_data
+        config_group = hf["config_data"]
+        config_data = read_dict_from_group(config_group)
+
+    return ekf_output_df, flight_data_df, config_data
+
+
+def read_dict_from_group(group):
+    config_dict = {}
+    for key, value in group.attrs.items():
+        if isinstance(value, bytes):
+            value = value.decode("utf-8")  # Decode byte strings back to regular strings
+        config_dict[key] = value
+
+    for subgroup_name in group:
+        subgroup = group[subgroup_name]
+        config_dict[subgroup_name] = read_dict_from_group(subgroup)
+
+    return config_dict
+
+
+results, flight_data, config_data = read_results("2019", "10", "08", "v3", addition="")
+mask = flight_data.cycle.isin([65])
+
+mask = mask & (flight_data.tether_reelout_speed > 0.2)
+
+flight_data = flight_data[mask]
+results = results[mask]
+results = results.reset_index(drop=True)
+flight_data = flight_data.reset_index(drop=True)
+
+# -----------------------------------------
+# Define the system
+# -----------------------------------------
+
+import json
+
+# -----------------------------------------------
+# Load data and define aerodynamic model
+# -----------------------------------------------
+
+# Define aerodynamic input
+file_path = "./data/v3_aero_input.json"
+with open(file_path, "r") as file:
+    aero_input = json.load(file)
+
+quasi_steady = True
+# -----------------------------------------------
+# Define the kite model
+# -----------------------------------------------
+kite_model = SystemModel(
+    mass_wing=15,
+    area_wing=19.75,
+    mass_kcu=28,
+    aero_input=aero_input,
+    dof=3,
+    quasi_steady=quasi_steady,
+)
+
+# -----------------------------------------------
+# Define the parametrized path
+# -----------------------------------------------
+r0 = np.linalg.norm(
+    [
+        flight_data["kite_position_x"].iloc[0],
+        flight_data["kite_position_y"].iloc[0],
+        flight_data["kite_position_z"].iloc[0],
+    ]
+)
+ry = 70
+rz = 25
+beta0 = np.radians(24)
+vr = np.mean(flight_data["tether_reelout_speed"])
+kite_model.speed_wind = np.mean(results["wind_speed_horizontal"])-1
+kite_model.input_depower = 0
+figure_eight = FigureEight(-1, r0, ry, rz * 2, vr, beta0, ky=0.5, kz=1, kappa=1)
+
+pattern = figure_eight
+
+
+
+start_state = {
+    "t": 0,
+    "s": 0,
+    "s_dot": 0.489,
+    "s_ddot": 0,
+    "tension_tether_ground": 1e3,
+    "input_steering": 0,
+    "angle_roll": 0,
+    "angle_pitch": 0,
+    "angle_yaw": 0,
+}
+time = np.arange(0, 100, 0.1)
+dof = 3
+
+
+# Run simulation
+phase = PhaseParameterized(kite_model, pattern, quasi_steady=quasi_steady)
+phase.run_simulation(start_state=start_state, time_array=time)
+# Extract variables
+s = phase.return_variable("s")
+s_dot = phase.return_variable("s_dot")
+
+
+fig, slider = phase.interactive_plot()
+
+vel_exp = np.array(
+    [
+        flight_data["kite_velocity_x"],
+        flight_data["kite_velocity_y"],
+        flight_data["kite_velocity_z"],
+    ]
+)[:,50:-50]
+pos_exp = np.array(
+    [
+        flight_data["kite_position_x"],
+        flight_data["kite_position_y"],
+        flight_data["kite_position_z"],
+    ]
+)[:,50:-50]
+print(vel_exp.shape)
+plt.figure()
+plt.plot(pos_exp[1,:]-np.mean(pos_exp[1,:]),pos_exp[2,:])
+plt.plot(phase.return_variable("y"), phase.return_variable("z"))
+
+# -----------------------------------------------
+# Display results
+# -----------------------------------------------
+
+v_exp = np.linalg.norm(vel_exp, axis=0)
+max_v_exp = max(v_exp)
+max_v = np.max(phase.return_variable("speed_tangential"))
+print(f"Max speed from flight data: {max_v_exp} m/s")
+print(f"Max speed from simulation: {max_v} m/s")
+print(f"Min speed from flight data: {np.min(v_exp)} m/s")
+print(f"Min speed from simulation: {np.min(phase.return_variable('speed_tangential'))} m/s")
+print(f"Mean speed from flight data: {np.mean(v_exp)} m/s")
+print(f"Mean speed from simulation: {np.mean(phase.return_variable('speed_tangential'))} m/s")
+
+print("----------------------------------------------------")
+
+print(f"Mean exp lift coefficient: {np.mean(results['wing_lift_coefficient'])}")
+print(f"Mean epx drag coefficient: {np.mean(results['wing_drag_coefficient'])}")
+print(f"Mean lift coefficient: {np.mean(phase.return_variable('lift_coefficient'))}")
+print(f"Mean drag coefficient: {np.mean(phase.return_variable('drag_coefficient'))}")
+print(f"Mean exp angle of attack: {np.mean(results['wing_angle_of_attack_bridle'])}")
+print(f"Mean angle of attack: {np.mean(phase.return_variable('angle_of_attack'))*180/np.pi}")
+
+time_arr = np.array(results["time"][50:-50])
+plt.figure()
+plt.plot(phase.return_variable("t"), phase.return_variable("speed_tangential"))
+plt.plot(time_arr-time_arr[0], v_exp)
+plt.plot(time_arr-time_arr[0], flight_data["tether_reelout_speed"][50:-50])
+
+plt.show()
