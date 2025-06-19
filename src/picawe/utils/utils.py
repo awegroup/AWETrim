d45e2dde44257bf2e4bdea2b020d27063342249c
ocayon <oriol.cayon@gmail.com>
2025-06-19 15:50:12 +0200

    Add functions to read EKF results

--- src/picawe/utils/utils.py
+++ src/picawe/utils/utils.py
@@ -1,14 +1,17 @@
 import casadi as ca
+import h5py
+import pandas as pd
+
 
 def skew_symmetric(v):
     return ca.vertcat(
         ca.horzcat(0, -v[2], v[1]),
         ca.horzcat(v[2], 0, -v[0]),
-        ca.horzcat(-v[1], v[0], 0)
+        ca.horzcat(-v[1], v[0], 0),
     )
 
-def calculate_angle_2vec(vector_a, vector_b, reference_vector=None):
 
+def calculate_angle_2vec(vector_a, vector_b, reference_vector=None):
 
     dot_product = ca.dot(vector_a, vector_b)
     magnitude_a = ca.norm_2(vector_a)
@@ -18,3 +21,62 @@ def calculate_angle_2vec(vector_a, vector_b, reference_vector=None):
     angle_rad = ca.arccos(cos_theta)
 
     return angle_rad
+
+
+def read_dict_from_group(group):
+    config_dict = {}
+    for key, value in group.attrs.items():
+        if isinstance(value, bytes):
+            value = value.decode("utf-8")  # Decode byte strings back to regular strings
+        config_dict[key] = value
+
+    for subgroup_name in group:
+        subgroup = group[subgroup_name]
+        config_dict[subgroup_name] = read_dict_from_group(subgroup)
+
+    return config_dict
+
+
+def read_ekf_results(year, month, day, kite_model, addition="", path_to_main=""):
+    path = "data/LEI-V9-KITE/flight_logs/"
+    date = str(year) + "-" + str(month) + "-" + str(day)
+    file_name = str(kite_model) + "_" + date
+    hdf5_path = path_to_main + path + file_name + addition + ".h5"
+    ekf_output_df, flight_data_df, config_data = read_results_from_hdf5(hdf5_path)
+    return ekf_output_df, flight_data_df, config_data
+
+
+def read_results_from_hdf5(hdf5_path):
+    with h5py.File(hdf5_path, "r") as hf:
+        # Read the ekf_output_df DataFrame
+        ekf_group = hf["ekf_output"]
+        ekf_output_df = pd.DataFrame(
+            {
+                col: (
+                    ekf_group[col][:].astype(str)
+                    if ekf_group[col].dtype.kind == "S"
+                    else ekf_group[col][:]
+                )
+                for col in ekf_group.keys()
+            }
+        )
+
+        # Read the flight_data DataFrame
+        flight_group = hf["flight_data"]
+        flight_data_df = pd.DataFrame(
+            {
+                col: (
+                    flight_group[col][:].astype(str)
+                    if flight_group[col].dtype.kind == "S"
+                    else flight_group[col][:]
+                )
+                for col in flight_group
+                if isinstance(flight_group[col], h5py.Dataset)
+            }
+        )
+
+        # Read config_data
+        config_group = hf["config_data"]
+        config_data = read_dict_from_group(config_group)
+
+    return ekf_output_df, flight_data_df, config_data
