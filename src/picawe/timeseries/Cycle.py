13106cc81ec61652ddf548cf797e95c25a297272
ocayon <oriol.cayon@gmail.com>
2025-06-19 15:53:20 +0200

    Improved reel-in (might fail still)

--- src/picawe/timeseries/Cycle.py
+++ src/picawe/timeseries/Cycle.py
@@ -6,31 +6,29 @@ from picawe.system.tether import RigidLumpedTether
 from picawe import SystemModel, State
 from picawe.environment.Wind import Wind
 
+
 class Cycle:
     def __init__(self, aero_input, sim_config):
         self.aero_input = aero_input
         self.sim_config = sim_config
-        self.wind_model = Wind(
-            wind_model=sim_config["wind_model"],
-            z0=sim_config["z0"]
-        )
+        self.wind_model = Wind(wind_model=sim_config["wind_model"], z0=sim_config["z0"])
         self.wind_model.speed_friction = sim_config["speed_friction"]
         self.kite = Kite(
             mass_wing=sim_config["mass_wing"],
             area_wing=sim_config["area_wing"],
             mass_kcu=sim_config["mass_kcu"],
             aero_input=aero_input,
-            steering_control=sim_config.get("steering_control", "roll")
+            steering_control=sim_config.get("steering_control", "roll"),
         )
         self.tether = RigidLumpedTether(diameter=sim_config["tether_diameter"])
 
-    def create_model(self):
+    def create_model(self, quasi_steady=True):
         model = SystemModel(
             dof=self.sim_config["dof"],
-            quasi_steady=self.sim_config["quasi_steady"],
+            quasi_steady=quasi_steady,
             kite=self.kite,
             wind_model=self.wind_model,
-            tether=self.tether
+            tether=self.tether,
         )
         model.wind.speed_friction = self.sim_config["speed_friction"]
         return model
@@ -38,14 +36,20 @@ class Cycle:
     def run_cycle(self, cycle_settings):
         pattern_config = cycle_settings["reelout"]
         model_ro = self.create_model()
-        phase_ro = PhaseParameterized(model_ro, quasi_steady=self.sim_config["quasi_steady"], pattern_config=pattern_config)
+        phase_ro = PhaseParameterized(
+            model_ro,
+            quasi_steady=cycle_settings["reelout"]["quasi_steady"],
+            pattern_config=pattern_config,
+        )
         print("Running reelout...")
         t0 = time.time()
         phase_ro.run_simulation()
         print("Reelout time:", time.time() - t0, "seconds")
 
-        model_ri = self.create_model()
-        phase_ri = ReelinPhase(model_ri, quasi_steady=self.sim_config["quasi_steady"])
+        model_ri = self.create_model(
+            quasi_steady=cycle_settings["reelin"]["quasi_steady"]
+        )
+        phase_ri = ReelinPhase(model_ri)
 
         init = cycle_settings["reelin"]["initial_state"]
         start_state_ri = State(
@@ -54,20 +58,29 @@ class Cycle:
             angle_elevation=phase_ro.return_variable("angle_elevation")[-1],
             angle_azimuth=phase_ro.return_variable("angle_azimuth")[-1],
             angle_course=phase_ro.return_variable("angle_course")[-1],
-            input_steering= phase_ro.return_variable("input_steering")[-1],
-            input_depower= phase_ro.return_variable("input_depower")[-1],
+            input_steering=phase_ro.return_variable("input_steering")[-1],
+            input_depower=phase_ro.return_variable("input_depower")[-1],
             speed_tangential=phase_ro.return_variable("speed_tangential")[-1],
-            timeder_angle_course= phase_ro.return_variable("timeder_angle_course")[-1],
+            timeder_angle_course=phase_ro.return_variable("timeder_angle_course")[-1],
             speed_radial=phase_ro.return_variable("speed_radial")[-1],
-            tension_tether_ground= phase_ro.return_variable("tension_tether_ground")[-1],
+            tension_tether_ground=phase_ro.return_variable("tension_tether_ground")[-1],
+            timeder_speed_tangential=phase_ro.return_variable(
+                "timeder_speed_tangential"
+            )[-1],
         )
 
-        cycle_settings["reelin"]["control"]["riro_elevation"] = phase_ro.return_variable("angle_elevation")[0]
-        cycle_settings["reelin"]["control"]["riro_azimuth"] = phase_ro.return_variable("angle_azimuth")[0]
+        cycle_settings["reelin"]["control"]["riro_elevation"] = (
+            phase_ro.return_variable("angle_elevation")[0]
+        )
+        cycle_settings["reelin"]["control"]["riro_azimuth"] = phase_ro.return_variable(
+            "angle_azimuth"
+        )[0]
 
         print("Running reelin...")
         t0 = time.time()
-        phase_ri.run_simulation(start_state=start_state_ri, settings=cycle_settings["reelin"])
+        phase_ri.run_simulation(
+            start_state=start_state_ri, settings=cycle_settings["reelin"]
+        )
         print("Reelin time:", time.time() - t0, "seconds")
 
         return phase_ro, phase_ri
